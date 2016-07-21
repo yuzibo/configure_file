b00ef0caca01c7a1b30dae7ec96e3af52c26f583
Bo Yu <tsu.yubo@gmail.com>
2016-07-21 18:52:14 +0800

    add three file.

---
+++ fetch.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: fetch.sh
+# Author: Bo Yu
+# mail: tsu.yubo@gmail.com
+# Created Time: 2016年07月21日 星期四 18时46分49秒
+##########################################################################
+#!/bin/bash
+git fetch origin && git rebase origin/master
