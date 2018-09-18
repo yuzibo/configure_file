46e980b1f125a62753aa66ad8a47bb3cde98112f
Bo YU <tsu.yubo@gmail.com>
2018-09-18 15:13:09 +0800

    add git auto script
    Signed-off-by: Bo YU <tsu.yubo@gmail.com>

---
+++ git/auto_build_git_config.sh
@@ -0,0 +1,14 @@
+#########################################################################
+# File Name: auto_build_git_config.sh
+# Author: Bo Yu
+# mail: tsu.yubo@gmail.com
+# Created Time: Tuesday, September 18, 2018 PM03:09:23 HKT
+##########################################################################
+#!/bin/bash
+
+git config --global core.editor "vim"
+
+git config --global user.name "Bo YU"
+git config --global user.email "tsu.yubo@gmail.com"
+
+git config --list
