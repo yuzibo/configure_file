c95453a221a6bee51c9cb64d1c556c631abc5055
Bo YU <tsu.yubo@gmail.com>
2018-11-17 19:57:23 +0800

    add taglist

---
+++ vim/buildtaglist.sh
@@ -0,0 +1,12 @@
+#########################################################################
+# File Name: buildtaglist.sh
+# Author: Bo Yu
+# mail: tsu.yubo@gmail.com
+# Created Time: 2018年11月17日 星期六 19时45分17秒
+##########################################################################
+#!/bin/bash
+
+mkdir -p ~/.vim/plugin/
+mkdir -p ~/.vim/doc/
+cp taglist/taglist.vim ~/.vim/plugin/
+cp taglist/taglist.txt ~/.vim/doc/
---
+++ vim/taglist/README.md
@@ -0,0 +1,13 @@
+---
+title: vim插件
+category: tools
+layout: post
+---
+* content
+{:toc}
+
+Taglist usage:
+
+[article](https://www.vim.org/scripts/script.php?script_id=273)
+
+Fast, quickly move the plugin files into local machine.
