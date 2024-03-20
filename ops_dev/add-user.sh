f5732a7932e79b81b41225af89e043ba78f6cad8
Bo YU <tsu.yubo@gmail.com>
2024-03-20 13:49:08 +0800

    update this scripts
    Signed-off-by: Bo YU <tsu.yubo@gmail.com>

--- ops_dev/add-user.sh
+++ ops_dev/add-user.sh
@@ -1,29 +1,35 @@
-#########################################################################
-# File Name: add-user.sh
-# Author: Bo Yu
-# mail: tsu.yubo@gmail.com
-# Created Time: Sat 12 Nov 2022 06:36:16 PM
-##########################################################################
 #!/bin/bash
 
 set -e
-u="vimer"
 
-# fill with your ssh pub id
-ssh_key="$(ssh-pub.id)"
-uh=/home/$u
-sshdir=$uh/.ssh
-authfile=$sshdir/authorized_keys
-echo "u = [[$u]], ssh_pub = [[$ssh_key]]"
-date | md5sum | base64
-#read -p 'is it looks good?'
-[ -d $uh ] || adduser $u --disabled-password
-mkdir $sshdir || true
-chmod 700 $sshdir
-echo ${ssh_key} >> $authfile
-chmod 600 $authfile
-chown -R $u.$u $sshdir
-ls -la $sshdir
-echo "Done for $u , $ssh_key"
+# pubkeys in txt like 'pubkey.debian-ci'
+keys_prefix="pubkey"
 
+
+for file in "$keys_prefix"*; do
+    if [ -f "$file" ]; then
+        echo "Found file: $file"
+    fi
+    [ -f "$file" ] || continue
+    u="${file#pubkey.}"
+    echo $u
+    u=`echo "$u" | tr '[:upper:]' '[:lower:]'`
+    uh=/home/$u
+    sshdir=$uh/.ssh
+    authfile=$sshdir/authorized_keys
+    echo "u = [[$u]], keys = [[$file]]"
+    date | md5sum | base64
+    read -p 'is it looks good?'
+    [ -d $uh ] || adduser $u --disabled-password
+    mkdir $sshdir || true
+    chmod 700 $sshdir
+    cat $file >> $authfile
+    chmod 600 $authfile
+    chown -R $u:$u $sshdir
+    ls -la $sshdir
+    echo "Done for $u , $file"
+    mv -vf "$file" "touched.$file"
+    #echo "#$u" >> /etc/ssh/sshd_config
+    read -p "ENTOR for next"
+done
 
